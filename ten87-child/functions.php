d5c3d00be66c33d9ab66f027ff9dbf3644797c2b
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-12 21:47:53 +0800

    Update functions.php

--- ten87-child/functions.php
+++ ten87-child/functions.php
@@ -8,7 +8,10 @@ if (!function_exists('obsius_child_theme_enqueue_scripts')) {
 	{
 		$main_style = 'obsius-main';
 
+		wp_enqueue_style('obsius-swiper-style', 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css')
 		wp_enqueue_style('obsius-child-style', get_stylesheet_directory_uri() . '/style.css', array($main_style));
+		wp_enqueue_script('obsius-swiper-script', 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js')
+		
 	}
 
 	add_action('wp_enqueue_scripts', 'obsius_child_theme_enqueue_scripts');

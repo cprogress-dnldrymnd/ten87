157bef69024207855cb61e1bb14d83a8e020c5ba
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-12 21:46:18 +0800

    cc

--- ten87-child/functions.php
+++ ten87-child/functions.php
@@ -1,17 +1,35 @@
 <?php
 
-if ( ! function_exists( 'obsius_child_theme_enqueue_scripts' ) ) {
+if (!function_exists('obsius_child_theme_enqueue_scripts')) {
 	/**
 	 * Function that enqueue theme's child style
 	 */
-	function obsius_child_theme_enqueue_scripts() {
+	function obsius_child_theme_enqueue_scripts()
+	{
 		$main_style = 'obsius-main';
 
-		wp_enqueue_style( 'obsius-child-style', get_stylesheet_directory_uri() . '/style.css', array( $main_style ) );
+		wp_enqueue_style('obsius-child-style', get_stylesheet_directory_uri() . '/style.css', array($main_style));
 	}
 
-	add_action( 'wp_enqueue_scripts', 'obsius_child_theme_enqueue_scripts' );
+	add_action('wp_enqueue_scripts', 'obsius_child_theme_enqueue_scripts');
 }
 
 require_once('includes/post-types.php');
 require_once('includes/shortcodes.php');
+
+
+function action_wp_footer()
+{
+?>
+	<script>
+		var swiper = new Swiper(".mySwiperPostSlider", {
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+	</script>
+<?php
+}
+
+add_action('wp_footer', 'action_wp_footer');

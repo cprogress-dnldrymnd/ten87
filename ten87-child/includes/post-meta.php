92084e10bfa42c254fd787a61372fcfedd394aa1
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 03:17:00 +0800

    Update post-meta.php

--- ten87-child/includes/post-meta.php
+++ ten87-child/includes/post-meta.php
@@ -8,11 +8,13 @@ use Carbon_Fields\Field;
 /* Studio
 /*-----------------------------------------------------------------------------------*/
 
-Container::make('post_meta', __('Page Title Settings'))
+Container::make('post_meta', __('Page Heading Settings'))
 	->where('post_type', '=', 'page')
 	->add_fields(
 		array(
-			Field::make('text', 'level', __('Level')),
-			Field::make('text', 'award', __('Award')),
+			Field::make('checkbox', 'show_custom_page_heading', __('Show custom page heading')),
+			Field::make('text', 'alt_title', __('Alt title')),
+			Field::make('text', 'description', __('Description'))
+			->set_help_text('Leave blank to use excerpt'),
 		)
 	);

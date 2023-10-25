b72b8599c4ff4fb04bfabebfcbd16031f8c041ab
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 07:18:13 +0800

    Update list-item-on-hover.js

--- ten87-child/includes/elementor-widgets/list-item-on-hover/list-item-on-hover.js
+++ ten87-child/includes/elementor-widgets/list-item-on-hover/list-item-on-hover.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function () {
   document.querySelectorAll('.list-item-content li').textContent = "";
+
+  console.log(document.querySelectorAll('.list-item-content li').textContent);
   var listitem = document.getElementsByClassName('list-item');
   for (var i = 0; i < listitem.length; i++) {
     var list_index = listitem[i];

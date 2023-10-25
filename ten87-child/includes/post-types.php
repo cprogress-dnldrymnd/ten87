ba92151735392680f6af68430e2ee75f37fdd706
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-25 20:01:39 +0800

    Update post-types.php

--- ten87-child/includes/post-types.php
+++ ten87-child/includes/post-types.php
@@ -209,3 +209,11 @@ function archive_rewrite_rules()
 }
 
 add_action('init', 'archive_rewrite_rules');
+
+add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
+function prefix_disable_gutenberg($current_status, $post_type)
+{
+    // Use your post type key instead of 'product'
+    if ($post_type === 'studios') return false;
+    return $current_status;
+}

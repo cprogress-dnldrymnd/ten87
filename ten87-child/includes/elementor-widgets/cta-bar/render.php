95e548750d991783b2596341f0bc64b622c5774f
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-25 23:59:34 +0800

    Update render.php

--- ten87-child/includes/elementor-widgets/cta-bar/render.php
+++ ten87-child/includes/elementor-widgets/cta-bar/render.php
@@ -1,5 +1,9 @@
 <div class="cta-bar">
     <div class="container">
-        .
+        <div class="row">
+            <?php if ($settings['cta_text']) { ?>
+                <h2><?= $settings['cta_text'] ?></h2>
+            <?php } ?>
+        </div>
     </div>
 </div>

c16d15190898b0634fac2519f25ba346037d4dac
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 03:47:17 +0800

    cc

--- ten87-child/includes/elementor.php
+++ ten87-child/includes/elementor.php
@@ -3,23 +3,26 @@
 function register_new_widgets($widgets_manager)
 {
 
-    require_once(__DIR__  . '/elementor-widgets/image-slider/image-slider.php');
-    require_once(__DIR__  . '/elementor-widgets/cta-bar/cta-bar.php');
-	
-    $widgets_manager->register(new \Elementor_CTA_Bar());
-    $widgets_manager->register(new \Elementor_Image_Slider());
+	require_once(__DIR__ . '/elementor-widgets/image-slider/image-slider.php');
+	require_once(__DIR__ . '/elementor-widgets/cta-bar/cta-bar.php');
+	require_once(__DIR__ . '/elementor-widgets/list-item-on-hover/list-item-on-hover.php');
+
+	$widgets_manager->register(new \Elementor_CTA_Bar());
+	$widgets_manager->register(new \Elementor_Image_Slider());
+	$widgets_manager->register(new \Elementor_List_Item_On_Hover());
 }
 add_action('elementor/widgets/register', 'register_new_widgets');
 
-function add_elementor_widget_categories( $elements_manager ) {
+function add_elementor_widget_categories($elements_manager)
+{
 
 	$elements_manager->add_category(
 		'Ten87',
 		[
-			'title' => esc_html__( 'Ten87', 'textdomain' ),
-			'icon' => 'fa fa-plug',
+			'title' => esc_html__('Ten87', 'textdomain'),
+			'icon'  => 'fa fa-plug',
 		]
 	);
 
 }
-add_action( 'elementor/elements/categories_registered', 'add_elementor_widget_categories' );
+add_action('elementor/elements/categories_registered', 'add_elementor_widget_categories');

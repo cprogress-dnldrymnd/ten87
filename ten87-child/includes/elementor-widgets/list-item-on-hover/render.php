c2dad38c520ebfbeb5595fdcfb891f7fe3d865c8
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 03:52:33 +0800

    Update render.php

--- ten87-child/includes/elementor-widgets/list-item-on-hover/render.php
+++ ten87-child/includes/elementor-widgets/list-item-on-hover/render.php
@@ -1,3 +1,12 @@
 <div class="list-item-on-hover">
-  list item on hover
+  <?php
+  if ($settings['list']) {
+    echo '<dl>';
+    foreach ($settings['list'] as $item) {
+      echo '<dt class="elementor-repeater-item-' . esc_attr($item['_id']) . '">' . $item['list_title'] . '</dt>';
+      echo '<dd>' . $item['list_content'] . '</dd>';
+    }
+    echo '</dl>';
+  }
+  ?>
 </div>

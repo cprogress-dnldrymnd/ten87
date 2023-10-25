cfb842f19e66982f04e9904d11fecd7b8ae68e4a
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 07:58:49 +0800

    cc

--- ten87-child/includes/elementor-widgets/list-item-on-hover/list-item-on-hover.js
+++ ten87-child/includes/elementor-widgets/list-item-on-hover/list-item-on-hover.js
@@ -38,7 +38,7 @@ jQuery(document).ready(function () {
   } else {
     jQuery('.list-item-content').each(function (index, element) {
       $height = jQuery(this).outerHeight();
-      jQuery(this).css('height', $height + 'px');
+      jQuery(this).css('--height', $height + 'px');
     });
 
   }

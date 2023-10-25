73bb0b699315f2c775f818d959ce33f39c6775b0
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-25 19:03:46 +0800

    cc

--- ten87-child/header.php
+++ ten87-child/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
-
+	<?php do_action('elementor_header') ?>
 	<?php wp_head(); ?>
 </head>
 

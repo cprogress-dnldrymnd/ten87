b0b075269652e70da9e7773be77e6db2612286c5
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 03:15:38 +0800

    Update post-meta.php

--- ten87-child/includes/post-meta.php
+++ ten87-child/includes/post-meta.php
@@ -4,68 +4,12 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Complex_Container;
 use Carbon_Fields\Field;
 
-Container::make('theme_options', __('Theme Option'))
-	->add_fields(
-		array(
-			Field::make('complex', 'our_schools', 'Our Schools')
-				->add_fields(
-					array(
-						Field::make('text', 'school', __('School')),
-
-					)
-				)
-				->set_layout('tabbed-vertical')
-				->set_header_template('<%- school  %>'),
-
-
-		)
-	);
 /*-----------------------------------------------------------------------------------*/
-/* Slider
+/* Studio
 /*-----------------------------------------------------------------------------------*/
 
-Container::make('post_meta', __('Slider Properties'))
-	->where('post_type', '=', 'slider')
-	->add_fields(
-		array(
-			Field::make('complex', 'slides', 'Slides')
-				->add_fields(
-					array(
-						Field::make('image', 'image', __('Background Image')),
-						Field::make('text', 'heading', __('Heading')),
-						Field::make('textarea', 'description', __('Description')),
-						Field::make('text', 'button_text_1', __('Button Text[1'))->set_width(50),
-						Field::make('text', 'button_link_1', __('Button Link[1'))->set_width(50),
-						Field::make('text', 'button_text_2', __('Button Text[1'))->set_width(50),
-						Field::make('text', 'button_link_2', __('Button Link[2'))->set_width(50),
-
-					)
-				)
-				->set_layout('grid')
-				->set_header_template('<%- heading  %>'),
-
-
-		)
-	);
-
-
-/*-----------------------------------------------------------------------------------*/
-/* Courses Categories
-/*-----------------------------------------------------------------------------------*/
-
-Container::make('term_meta', __('Category Properties'))
-	->where('term_taxonomy', '=', 'course-category')
-	->add_fields(array(
-		Field::make('textarea', 'svg_icon', __('SVG Icon')),
-	));
-
-
-/*-----------------------------------------------------------------------------------*/
-/* Slider
-/*-----------------------------------------------------------------------------------*/
-
-Container::make('post_meta', __('Course Properties'))
-	->where('post_type', '=', 'course')
+Container::make('post_meta', __('Page Title Settings'))
+	->where('post_type', '=', 'page')
 	->add_fields(
 		array(
 			Field::make('text', 'level', __('Level')),

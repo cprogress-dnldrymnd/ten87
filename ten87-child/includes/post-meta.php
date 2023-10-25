7b98aaa5d25a5fbe0c6cb525299aad2db4b81b74
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 03:18:14 +0800

    cc

--- ten87-child/includes/post-meta.php
+++ ten87-child/includes/post-meta.php
@@ -13,7 +13,8 @@ Container::make('post_meta', __('Page Heading Settings'))
 	->add_fields(
 		array(
 			Field::make('checkbox', 'show_custom_page_heading', __('Show custom page heading')),
-			Field::make('text', 'alt_title', __('Alt title')),
+			Field::make('text', 'alt_title', __('Alt title'))
+				->set_help_text('Leave blank to use title'),
 			Field::make('text', 'description', __('Description'))
 			->set_help_text('Leave blank to use excerpt'),
 		)

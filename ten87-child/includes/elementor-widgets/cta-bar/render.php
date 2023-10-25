3c160bcb01e71b5099ef7f3712d3ccc8153b5397
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 02:43:27 +0800

    cc

--- ten87-child/includes/elementor-widgets/cta-bar/render.php
+++ ten87-child/includes/elementor-widgets/cta-bar/render.php
@@ -1,14 +1,14 @@
 <div class="cta-bar">
     <div class="container">
+        <?php
+        if (!empty($settings['cta_url']['url'])) {
+            $this->add_link_attributes('cta_url', $settings['cta_url']);
+        }
+        if ($settings['cta_url']) {
+            echo '<a ' . $this->get_render_attribute_string('cta_url') . '>';
+        }
+        ?>
         <div class="row">
-            <?php
-            if (!empty($settings['cta_url']['url'])) {
-                $this->add_link_attributes('cta_url', $settings['cta_url']);
-            }
-            if ($settings['cta_url']) {
-                echo '<a ' . $this->get_render_attribute_string('cta_url') . '>';
-            }
-            ?>
             <?php if ($settings['cta_text']) { ?>
                 <div class="heading">
                     <h2><?= $settings['cta_text'] ?></h2>
@@ -17,11 +17,11 @@
             <div class="icon">
                 <img src="<?= $settings['icon']['value']['url'] ?>" alt="">
             </div>
-            <?php
-            if ($settings['cta_url']) {
-                echo '</a>';
-            }
-            ?>
         </div>
+        <?php
+        if ($settings['cta_url']) {
+            echo '</a>';
+        }
+        ?>
     </div>
 </div>

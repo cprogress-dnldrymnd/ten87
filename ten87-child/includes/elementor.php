a973c1f31ac321ee0b555c349e905830fb9d4d81
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-25 20:41:01 +0800

    Update elementor.php

--- ten87-child/includes/elementor.php
+++ ten87-child/includes/elementor.php
@@ -8,3 +8,16 @@ function register_new_widgets($widgets_manager)
     $widgets_manager->register(new \Elementor_Image_Slider());
 }
 add_action('elementor/widgets/register', 'register_new_widgets');
+
+function add_elementor_widget_categories( $elements_manager ) {
+
+	$elements_manager->add_category(
+		'Ten87',
+		[
+			'title' => esc_html__( 'Ten87', 'textdomain' ),
+			'icon' => 'fa fa-plug',
+		]
+	);
+
+}
+add_action( 'elementor/elements/categories_registered', 'add_elementor_widget_categories' );

27252ca9f46492cd3010fc99b5f703e5dafc7185
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 07:53:57 +0800

    Update list-item-on-hover.js

--- ten87-child/includes/elementor-widgets/list-item-on-hover/list-item-on-hover.js
+++ ten87-child/includes/elementor-widgets/list-item-on-hover/list-item-on-hover.js
@@ -1,31 +1,33 @@
 jQuery(document).ready(function () {
+  if (window.innerWidth > 992) {
 
-  var listitem = document.getElementsByClassName('list-item');
-  for (var i = 0; i < listitem.length; i++) {
-    var list_index = listitem[i];
-    $offset = list_index.offsetTop;
-    list_index.style.cssText = 'top: ' + $offset + 'px; --offset: -' + $offset + 'px';
+    var listitem = document.getElementsByClassName('list-item');
+    for (var i = 0; i < listitem.length; i++) {
+      var list_index = listitem[i];
+      $offset = list_index.offsetTop;
+      list_index.style.cssText = 'top: ' + $offset + 'px; --offset: -' + $offset + 'px';
+    }
+    var listparent = document.querySelectorAll('.list-item-content > ul');
+
+    listparent.forEach(function (item, index) {
+      var listitem = listparent[index].children
+      for (var i = 0; i < listitem.length; i++) {
+        var list_index = listitem[i];
+        $offset = list_index.offsetTop;
+        list_index.style.cssText = '--offset: -' + $offset + 'px; --transition-delay: ' + (i * 50) + 'ms; --transition-delay-hover: ' + ((listitem.length * 50) + (i * 50)) + 'ms';
+      }
+    });
+
+    var listparent = document.querySelectorAll('.list-item-content > ul > li > ul');
+    listparent.forEach(function (item, index) {
+      var listitem = listparent[index].children
+      for (var i = 0; i < listitem.length; i++) {
+        var list_index = listitem[i];
+        $offset = list_index.offsetTop;
+        list_index.style.cssText = '--transition-delay: ' + (i * 50) + 'ms; --transition-delay-hover: ' + ((listitem.length * 50) + (i * 50)) + 'ms';
+      }
+    });
   }
-  var listparent = document.querySelectorAll('.list-item-content > ul');
-
-  listparent.forEach(function (item, index) {
-    var listitem = listparent[index].children
-    for (var i = 0; i < listitem.length; i++) {
-      var list_index = listitem[i];
-      $offset = list_index.offsetTop;
-      list_index.style.cssText = '--offset: -' + $offset + 'px; --transition-delay: ' + (i * 50) + 'ms; --transition-delay-hover: ' + ((listitem.length * 50) + (i * 50)) + 'ms';
-    }
-  });
-
-  var listparent = document.querySelectorAll('.list-item-content > ul > li > ul');
-  listparent.forEach(function (item, index) {
-    var listitem = listparent[index].children
-    for (var i = 0; i < listitem.length; i++) {
-      var list_index = listitem[i];
-      $offset = list_index.offsetTop;
-      list_index.style.cssText = '--transition-delay: ' + (i * 50) + 'ms; --transition-delay-hover: ' + ((listitem.length * 50) + (i * 50)) + 'ms';
-    }
-  });
 
   if (window.innerWidth > 992) {
     jQuery('.list-item-box').each(function (index, element) {
@@ -38,6 +40,6 @@ jQuery(document).ready(function () {
       $height = jQuery(this).outerHeight();
       jQuery(this).css('height', $height + 'px');
     });
-    
+
   }
 });

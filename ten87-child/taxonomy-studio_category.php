4151f778d212c7b6fdb39e95a679ccc77ceb1398
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-25 19:31:19 +0800

    Update taxonomy-studio_category.php

--- ten87-child/taxonomy-studio_category.php
+++ ten87-child/taxonomy-studio_category.php
@@ -5,6 +5,42 @@ $title = $object->name;
 $desc = $object->description;
 ?>
 <div class="page-heading elementor-section elementor-section-boxed">
+    <div class="blob-holder-absolute blob-holder-1 blob-hero-1">
+        <!-- Draw everything relative to a 200x200 canvas, this will then scale to any resolution -->
+        <div class="blob-holder blob-box">
+            <svg viewBox="0 0 200 200">
+                <defs>
+
+                    <radialGradient id="gradient" gradientTransform="translate(-0.5 -0.5) scale(2, 2)">
+                        <stop offset="0%" stop-color="#ff5932"></stop>
+                        <stop offset="10%" stop-color="rgba(255, 89, 50, 0.75)"></stop>
+                        <stop offset="20%" stop-color="rgba(255, 89, 50, 0.5)"></stop>
+                        <stop offset="40%" stop-color="rgba(255, 89, 50, 0)"></stop>
+                    </radialGradient>
+                </defs>
+                <path d="" fill="url('#gradient')"></path>
+            </svg>
+
+        </div>
+    </div>
+    <div class="blob-holder-absolute blob-holder-2 blob-hero-2">
+        <!-- Draw everything relative to a 200x200 canvas, this will then scale to any resolution -->
+        <div class="blob-holder-2 blob-box">
+            <svg viewBox="0 0 200 200">
+                <defs>
+
+                    <radialGradient id="gradient" gradientTransform="translate(-0.5 -0.5) scale(2, 2)">
+                        <stop offset="0%" stop-color="#ff5932"></stop>
+                        <stop offset="10%" stop-color="rgba(255, 89, 50, 0.75)"></stop>
+                        <stop offset="20%" stop-color="rgba(255, 89, 50, 0.5)"></stop>
+                        <stop offset="40%" stop-color="rgba(255, 89, 50, 0)"></stop>
+                    </radialGradient>
+                </defs>
+                <path d="" fill="url('#gradient')"></path>
+            </svg>
+
+        </div>
+    </div>
     <div class="elementor-container">
         <div class="qodef-section-title">
             <h1 class="qodef-shortcode qodef-m  qodef-custom-font qodef-custom-font-223 qodef-layout--simple qodef-alignment--left">

beb404743eadefb1eba96ed67326f2adfbb737bd
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-25 18:30:34 +0800

    Update taxonomy-studio_category.php

--- ten87-child/taxonomy-studio_category.php
+++ ten87-child/taxonomy-studio_category.php
@@ -1,7 +1,7 @@
 <?php
 get_header();
 ?>
-<?= do_shortcode('[custom_template id=5791]'); ?>
+<?= do_shortcode('[custom_template post_id=5791]'); ?>
 <?php if (have_posts()) { ?>
     <?php while (have_posts()) {
         the_post() ?>

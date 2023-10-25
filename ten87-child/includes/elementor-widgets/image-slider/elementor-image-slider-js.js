fa0cef408f6192d0adb87a93682a49e58c963fe3
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-25 20:45:31 +0800

    Update elementor-image-slider-js.js

--- ten87-child/includes/elementor-widgets/image-slider/elementor-image-slider-js.js
+++ ten87-child/includes/elementor-widgets/image-slider/elementor-image-slider-js.js
@@ -1,12 +1,6 @@
 
 jQuery(document).ready(function () {
-    var mySwiperPartnerThumbImages = new Swiper(".mySwiperPartnerThumbImages", {
-        loop: true,
-        watchSlidesProgress: true,
-        slidesPerView: 1,
-    });
-
-    const mySwiperPartnernoThumb = new Swiper(".mySwiperPartnernoThumb", {
+    const swiperImageSlide = new Swiper(".swiperImageSlide", {
         loop: true,
         autoplay: true,
         slidesPerView: 1,
@@ -15,19 +9,5 @@ jQuery(document).ready(function () {
             clickable: true
         },
     });
-
-    const mySwiperPartnerThumb = new Swiper(".mySwiperPartnerThumb", {
-        loop: true,
-        autoplay: true,
-        slidesPerView: 1,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-        thumbs: {
-            swiper: mySwiperPartnerThumbImages,
-        },
-    });
-
 
 });

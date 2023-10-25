3778b5400d77bfc083e5a178c494f6759fad597f
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 00:04:01 +0800

    Update render.php

--- ten87-child/includes/elementor-widgets/cta-bar/render.php
+++ ten87-child/includes/elementor-widgets/cta-bar/render.php
@@ -1,9 +1,20 @@
 <div class="cta-bar">
     <div class="container">
         <div class="row">
+            <?php
+            if ( ! empty( $settings['cta_url']['url'] ) ) {
+                $this->add_link_attributes( 'cta_url', $settings['cta_url'] );
+            }
+            if ($settings['cta_url']['url']) {
+                echo '<a' . $this->get_render_attribute_string('cta_url');
+            }
+            ?>
             <?php if ($settings['cta_text']) { ?>
                 <h2><?= $settings['cta_text'] ?></h2>
             <?php } ?>
+            <div class="icon">
+
+            </div>
         </div>
     </div>
 </div>

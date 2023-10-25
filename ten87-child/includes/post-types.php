3726c3a1908b553c374cab765d29334f8da42152
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-25 18:07:59 +0800

    cc

--- ten87-child/includes/post-types.php
+++ ten87-child/includes/post-types.php
@@ -182,12 +182,8 @@ new newPostType(
 
 function wpa_studio_post_link( $post_link, $id = 0 ){
     $post = get_post($id);  
-    if ( is_object( $post ) ){
-        $terms = wp_get_object_terms( $post->ID, 'studio_category' );
-        if( $terms ){
-            return str_replace( '%studio_category%' , $terms[0]->slug , $post_link );
-        }
-    } 
+	return str_replace( '%studio_category%' , '' , $post_link );
+
     return $post_link;  
 }
 add_filter( 'post_type_link', 'wpa_studio_post_link', 1, 3 );

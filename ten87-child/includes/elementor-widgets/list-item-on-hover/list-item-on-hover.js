8be7161aab633779c03681c249bbe67e2f800cc5
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-26 06:41:12 +0800

    Update list-item-on-hover.js

--- ten87-child/includes/elementor-widgets/list-item-on-hover/list-item-on-hover.js
+++ ten87-child/includes/elementor-widgets/list-item-on-hover/list-item-on-hover.js
@@ -16,7 +16,7 @@ jQuery(document).ready(function () {
   for (var i = 0; i < listitem.length; i++) {
     var list_index = listitem[i];
     $offset = list_index.offsetTop;
-    list_index.style.cssText = '--transition-delay: ' + (i * 50) + 'ms; --transition-delay-hover: ' + (50 * listitem.length) + 'ms';
+    list_index.style.cssText = '--transition-delay: ' + (i * 50) + 'ms; --transition-delay-hover: ' + (50 * listitem.length) + 50 + 'ms';
   }
 
   jQuery('.list-item-box').each(function (index, element) {

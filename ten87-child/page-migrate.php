69d790b25f42ce8bea64001addfd21ef2324dca0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-16 19:10:13 +0800

    Update page-migrate.php

--- ten87-child/page-migrate.php
+++ ten87-child/page-migrate.php
@@ -1,5 +1,11 @@
 <?php get_header() ?>
 <div>
-    ssss
+    <?php
+    $client = new \Storyblok\Client('YOUR_OAUTH_TOKEN');
+
+    $client->get('cdn/stories', [
+        "token" =>  "Z5R6TMf4M0FDuypDqcwQIwtt"
+    ])->getBody();
+    ?>
 </div>
 <?php get_footer() ?>

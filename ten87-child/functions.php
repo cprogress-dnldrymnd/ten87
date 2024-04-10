bc31dccd20cd50f7c1b891be8236dfdbc8accefc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-10 21:39:55 +0800

    css

--- ten87-child/functions.php
+++ ten87-child/functions.php
@@ -175,15 +175,7 @@ function custom_class($classes)
 function action_obsius_action_before_body_tag_close()
 {
 ?>
-	<div class="modal-form-backdrop modal-close">
-		<div class="close-holder-wrapper">
-			<div class="close-holder">
-				<div class="modal-close">
-					&#10006;
-				</div>
-			</div>
-		</div>
-	</div>
+	<div class="modal-form-backdrop modal-close"></div>
 	<div class="modal-form" id="modal-form">
 		<div class="modal-holder">
 			<div class="inner">
@@ -210,7 +202,11 @@ function action_obsius_action_before_body_tag_close()
 								</div>
 							</div>
 						</div>
-
+						<div class="close-holder">
+							<div class="modal-close">
+								&#10006;
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>

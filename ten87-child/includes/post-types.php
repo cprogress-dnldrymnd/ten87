44e53eca7de8c5c3536c8f6f488c53137d74f7d7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-21 17:16:30 +0800

    Update post-types.php

--- ten87-child/includes/post-types.php
+++ ten87-child/includes/post-types.php
@@ -286,7 +286,7 @@ function post_query($query)
 
 	if (isset($query->query['post_type'])) {
 		if ($query->query['post_type'] === 'post') {
-			$query->set('orderby', 'menu_order');
+			$query->set('orderby', 'date menu_order');
 			$query->set('order', 'ASC');
 		}
 	}

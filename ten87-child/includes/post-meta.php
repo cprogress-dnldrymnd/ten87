a12fa4aef489c0fb128523d6cad018f2573dfbe4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 21:06:52 +0800

    Update post-meta.php

--- ten87-child/includes/post-meta.php
+++ ten87-child/includes/post-meta.php
@@ -91,11 +91,11 @@ Container::make('post_meta', __('Template Options'))
 	);
 
 
-Container::make('post_meta', __('Page Heading Settings'))
+Container::make('post_meta', __(' Settings'))
 	->where('post_type', '=', 'fundings')
 	->add_fields(
 		array(
 			Field::make('checkbox', 'has_readmore', __('Has Readmore'))
-			->set_help_text('Check this to display readmore in popup.'),
+				->set_help_text('Check this to display readmore in popup.'),
 		)
 	);

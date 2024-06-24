80966d6204ef2516d8cf257614a4686100b94b9d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-24 21:45:13 +0800

    Update single-rate-cards.php

--- ten87-child/single-rate-cards.php
+++ ten87-child/single-rate-cards.php
@@ -5,6 +5,18 @@
     $media = get__post_meta('media');
     ?>
     <section class="single-rate-cards">
+        <?php if ($media) { ?>
+            <?php
+            $mime_type = get_post_mime_type($media);
+            ?>
+            <div class="media">
+                <?php if (str_contains($mime_type, 'image')) { ?>
+                    <img src="<?= wp_get_attachment_image_url($media, 'large') ?>">
+                <?php } else if (str_contains($mime_type, 'video')) { ?>
+                    <video src="<?= wp_get_attachment_url($media) ?>" controls autoplay muted></video>
+                <?php } ?>
+            </div>
+        <?php } ?>
         <div class="container">
             <div class="row">
                 <div class="col featured-image">
@@ -14,18 +26,7 @@
                     <?php the_content() ?>
                 </div>
             </div>
-            <?php if ($media) { ?>
-                <?php
-                $mime_type = get_post_mime_type($media);
-                ?>
-                <div class="media">
-                    <?php if (str_contains($mime_type, 'image')) { ?>
-                        <img src="<?= wp_get_attachment_image_url($media, 'large') ?>">
-                    <?php } else if (str_contains($mime_type, 'video')) { ?>
-                        <video src="<?= wp_get_attachment_url($media) ?>" controls autoplay muted></video>
-                    <?php } ?>
-                </div>
-            <?php } ?>
+
         </div>
     </section>
 <?php } ?>

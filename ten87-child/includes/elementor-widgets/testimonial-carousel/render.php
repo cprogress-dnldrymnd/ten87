232e937e1a8babdc8b5b4219ce58f38820ac0cb2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 18:53:43 +0800

    css

--- ten87-child/includes/elementor-widgets/testimonial-carousel/render.php
+++ ten87-child/includes/elementor-widgets/testimonial-carousel/render.php
@@ -3,11 +3,38 @@ $number_of_slides_desktop = $settings['number_of_slides_desktop'];
 $number_of_slides_tablet = $settings['number_of_slides_tablet'];
 $number_of_slides_mobile = $settings['number_of_slides_mobile'];
 $slide_duration = $settings['slide_duration'];
+$testimonials = get_posts(array(
+    'post_type' => 'testimonials',
+    'numberposts' => -1,
+));
 ?>
 <div class="testimonial-carousel-holder">
     <div class="swiper swiper-testimonial-carousel" number_of_slides_desktop="<?= $number_of_slides_desktop ?>" number_of_slides_tablet="<?= $number_of_slides_tablet ?>" number_of_slides_mobile="<?= $number_of_slides_mobile ?>" slide_duration="<?= $slide_duration ?>">
         <div class="swiper-wrapper">
-
+            <?php foreach ($testimonials as $testimonial) { ?>
+                <?php
+                $qodef_testimonials_text = get_post_meta($testimonial->ID, 'qodef_testimonials_text', true);
+                ?>
+                <div class="swiper-slide">
+                    <div class="image-box">
+                        <?= get_the_post_thumbnail('large') ?>
+                    </div>
+                    <div class="content-box">
+                        <?= wpautop($qodef_testimonials_text) ?>
+                        <div class="author">
+                            <?= $testimonial->post_title ?>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+        <div class="swiper-navigation">
+            <div class="swiper-button-prev swiper-button-outside swiper-button-prev-553" tabindex="0" role="button" aria-label="Previous slide">
+                <img decoding="async" src="https://ten87.studio/wp-content/themes/ten87-child/assets/images/arrow-black.svg" alt="" style="border-bottom-color: rgba(0, 0, 0, 0);">
+            </div>
+            <div class="swiper-button-next swiper-button-outside swiper-button-next-553" tabindex="0" role="button" aria-label="Next slide">
+                <img decoding="async" src="https://ten87.studio/wp-content/themes/ten87-child/assets/images/arrow-black.svg" alt="" style="border-bottom-color: rgba(0, 0, 0, 0);">
+            </div>
         </div>
     </div>
 </div>
--- ten87-child/includes/elementor-widgets/testimonial-carousel/testimonial-carousel.js
+++ ten87-child/includes/elementor-widgets/testimonial-carousel/testimonial-carousel.js
@@ -1,4 +1,39 @@
 jQuery(document).ready(function () {
+    jQuery('.swiper-testimonial-carousel').each(function (index, element) {
+        $number_of_slides_desktop = jQuery(this).attr('number_of_slides_desktop');
+        $number_of_slides_tablet = jQuery(this).attr('number_of_slides_tablet');
+        $number_of_slides_mobile = jQuery(this).attr('number_of_slides_mobile');
+        $slide_duration = jQuery(this).attr('slide_duration');
 
-    console.log('xxx')
+        var swiper_testimonial_carousel = new Swiper(".swiper-testimonial-carousel", {
+            loop: true,
+            spaceBetween: 30,
+            autoplay: {
+                delay: $slide_duration,
+                disableOnInteraction: true,
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: $number_of_slides_mobile,
+                },
+
+                768: {
+                    slidesPerView: $number_of_slides_tablet,
+                },
+
+                992: {
+                    slidesPerView: $number_of_slides_desktop,
+                },
+
+
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+
+
+        });
+
+    });
+
 });

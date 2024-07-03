5096c4836a98e847be2960045e932b366985b7e7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 19:38:46 +0800

    Update testimonial-carousel.js

--- ten87-child/includes/elementor-widgets/testimonial-carousel/testimonial-carousel.js
+++ ten87-child/includes/elementor-widgets/testimonial-carousel/testimonial-carousel.js
@@ -1,46 +1,43 @@
-jQuery(document).ready(function () {
-    jQuery('.swiper-testimonial-carousel').each(function (index, element) {
-        $number_of_slides_desktop = jQuery(this).attr('number_of_slides_desktop');
-        $number_of_slides_tablet = jQuery(this).attr('number_of_slides_tablet');
-        $number_of_slides_mobile = jQuery(this).attr('number_of_slides_mobile');
-        $slide_duration = jQuery(this).attr('slide_duration');
+$number_of_slides_desktop = jQuery(this).attr('number_of_slides_desktop');
+$number_of_slides_tablet = jQuery(this).attr('number_of_slides_tablet');
+$number_of_slides_mobile = jQuery(this).attr('number_of_slides_mobile');
+$slide_duration = jQuery(this).attr('slide_duration');
 
-        var swiper = new Swiper(".swiper-testimonial-carousel", {
-            loop: true,
-            spaceBetween: 30,
-            autoplay: {
-                delay: $slide_duration,
-                disableOnInteraction: false,
-                pauseOnMouseEnter: true,
-            },
-            breakpoints: {
-                0: {
-                    slidesPerView: $number_of_slides_mobile,
-                },
+var swiper = new Swiper(".swiper-testimonial-carousel", {
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: $slide_duration,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: $number_of_slides_mobile,
+        },
 
-                768: {
-                    slidesPerView: $number_of_slides_tablet,
-                },
+        768: {
+            slidesPerView: $number_of_slides_tablet,
+        },
 
-                992: {
-                    slidesPerView: $number_of_slides_desktop,
-                },
+        992: {
+            slidesPerView: $number_of_slides_desktop,
+        },
 
 
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-
-        });
-
-        jQuery(".swiper-testimonial-carousel").hover(function () {
-            (this).swiper.autoplay.stop();
-        }, function () {
-            (this).swiper.autoplay.start();
-        });
-
-    });
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
 });
+
+jQuery(".swiper-testimonial-carousel").hover(function () {
+    (this).swiper.autoplay.stop();
+    console.log('xx');
+}, function () {
+    (this).swiper.autoplay.start();
+    console.log('xx2');
+});
+

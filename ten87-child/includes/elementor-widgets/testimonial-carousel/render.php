e57937a96678bb0dc63213a591e6d9ea9ec88324
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 19:03:04 +0800

    Update render.php

--- ten87-child/includes/elementor-widgets/testimonial-carousel/render.php
+++ ten87-child/includes/elementor-widgets/testimonial-carousel/render.php
@@ -23,6 +23,9 @@ $testimonials = get_posts(array(
                             </div>
                         </div>
                         <div class="content-box">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="81.529" height="67.704" viewBox="0 0 81.529 67.704">
+                                <path id="Icon_metro-quote" data-name="Icon metro-quote" d="M0,32.852V65.7H34.238V32.852H17.119s0-16.426,17.119-16.426V0S0,0,0,32.852ZM79.529,16.426V0S45.292,0,45.292,32.852V65.7H79.529V32.852H62.411S62.411,16.426,79.529,16.426Z" transform="translate(1 1)" fill="none" stroke="#000" stroke-width="2" />
+                            </svg>
                             <?= wpautop($qodef_testimonials_text) ?>
                             <div class="author">
                                 <?= $testimonial->post_title ?>

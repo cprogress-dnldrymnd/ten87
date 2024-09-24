543edb4ae41725fd2567ac00447da9a80cd3ae4b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-24 16:16:16 +0800

    Update taxonomy-studio_category.php

--- ten87-child/taxonomy-studio_category.php
+++ ten87-child/taxonomy-studio_category.php
@@ -1,33 +1,40 @@
 <?php
 get_header();
-    $args = array(
-        'numberposts' => -1,
-        'post_type' => 'studios',
-        'orderby'    => 'menu_order',
-        'order' => 'asc'
-    );
+$args = array(
+    'numberposts' => -1,
+    'post_type' => 'studios',
+    'orderby'    => 'menu_order',
+    'order' => 'asc',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'studio_category',
+            'field' => 'term_id',
+            'terms' => get_queried_object()->term_id,
+        ),
+    )
+);
 
-    $posts = get_posts($args);
+$posts = get_posts($args);
 
 ?>
-    <div class="post-slider-holder">
-        <div class="swiper mySwiperPostSlider">
-            <div class="swiper-wrapper">
-                <?php foreach ($posts as $post) { ?>
-                    <?php
-                    $args = array(
-                        'bg' => get_the_post_thumbnail_url($post->ID, 'full'),
-                        'title' => $post->post_title,
-                        'post_excerpt' => $post->post_excerpt,
-                        'post_id' => $post->ID
-                    );
+<div class="post-slider-holder">
+    <div class="swiper mySwiperPostSlider">
+        <div class="swiper-wrapper">
+            <?php foreach ($posts as $post) { ?>
+                <?php
+                $args = array(
+                    'bg' => get_the_post_thumbnail_url($post->ID, 'full'),
+                    'title' => $post->post_title,
+                    'post_excerpt' => $post->post_excerpt,
+                    'post_id' => $post->ID
+                );
 
-                    get_template_part('template-parts/studio/studio-swiper-slide', 'null', $args);
-                    ?>
-                <?php } ?>
-            </div>
-            <?= swiper_navigation() ?>
+                get_template_part('template-parts/studio/studio-swiper-slide', 'null', $args);
+                ?>
+            <?php } ?>
         </div>
+        <?= swiper_navigation() ?>
     </div>
+</div>
 
 <?php get_footer() ?>
